8ffd2e7b67e85b90ab8a97419b9d487a145b7aae
Yash Sanghavi <y.sanghavi001@gmail.com>
2022-01-26 20:23:42 +0530

    Integration of react leaflet and api calling.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
+import { useQuery } from "react-query";
+// eslint-disable-next-line no-unused-vars
+import { MapContainer, Marker, Polygon, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 function App() {
+  const { isLoading, error, data } = useQuery("areaData", () =>
+    axios.get("https://kyupid-api.vercel.app/api/areas")
+  );
+  const {
+    isLoading: isLoading2,
+    error: error2,
+    data: data2,
+  } = useQuery("userData", () =>
+    axios.get("https://kyupid-api.vercel.app/api/users")
+  );
+  useEffect(() => {
+    console.log("hello");
+  });
+
+  console.log("DATA:", data);
+  console.log("ERROR:", error);
+  console.log("LOADING:", isLoading);
+
+  console.log("DATA2:", data2);
+  console.log("ERROR2:", error2);
+  console.log("LOADING2:", isLoading2);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome To Dating App Resolution</h1>
+      <MapContainer
+        style={{ height: "80vh" }}
+        zoom={11}
+        center={[12.971599, 77.594566]}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {data?.data?.features.map((value) => {
+          const coordinates = value.geometry.coordinates[0].map((item) => [
+            item[1],
+            item[0],
+          ]);
+          console.log("VALUE:", value.geometry.coordinates);
+          return (
+            <Polygon
+              pathOptions={{
+                fillColor: "#FD8D3C",
+                fillOpacity: 0.7,
+                weight: 2,
+                opacity: 1,
+                dashArray: 3,
+                color: "white",
+              }}
+              positions={coordinates}
+              eventHandlers={{
+                mouseover: (e) => {
+                  const layer = e.target;
+                  layer.setStyle({
+                    fillOpacity: 0.7,
+                    weight: 5,
+                    dashArray: "3",
+                    color: "#666",
+                    fillColor: "#D45962",
+                  });
+                },
+                mouseout: (e) => {
+                  const layer = e.target;
+                  layer.setStyle({
+                    fillOpacity: 0.7,
+                    weight: 2,
+                    dashArray: "3",
+                    color: "white",
+                    fillColor: "#FD8D3C",
+                  });
+                },
+                click: (e) => {},
+              }}
+            />
+          );
+        })}
+        {/* <Marker position={[12.971599, 77.594566]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker> */}
+      </MapContainer>
     </div>
   );
 }
